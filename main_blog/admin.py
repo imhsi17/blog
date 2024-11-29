bf68414e3c69440e256d860eb5fb10a6d21460e2
imhsi17 <calderonhuaynates@gmail.com>
2024-11-29 16:12:36 -0500

    Registro de los modelos en el panel de administración.

--- main_blog/admin.py
+++ main_blog/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+@admin.register(Lector)
+class LectorAdmin(admin.ModelAdmin):
+    
+    list_display = ('nombre', 'apellido', 'correo', 'fecha_registro', 'estado')
+    list_filter = ('estado',)
+    list_per_page = 25
+    search_fields = ['nombre', 'apellido']
+
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    
+    list_display = ('nombre', 'estado', 'fecha_registro', 'fecha_modificacion')
+    list_filter = ('estado',)
+    list_per_page = 25
+    search_fields = ['nombre']
+
+
+@admin.register(Publicacion)
+class PublicacionAdmin(admin.ModelAdmin):
+    
+    list_display = ('titulo', 'contenido', 'estado', 'autor', 'categoria','fecha_publicacion')
+    list_filter = ('autor', 'categoria', 'estado')
+    list_per_page = 20
+    search_fields = ['titulo']
+
+
+@admin.register(ComentarioAutor)
+class ComentarioAutorAdmin(admin.ModelAdmin):
+    
+    list_display = ('contenido', 'autor', 'publicacion', 'fecha_publicacion')
+    list_filter = ('autor', 'publicacion')
+    list_per_page = 30
+
+
+@admin.register(ComentarioAutor)
+class ComentarioLectorAdmin(admin.ModelAdmin):
+    
+    list_display = ('contenido', 'lector', 'publicacion', 'fecha_publicacion')
+    list_filter = ('lector', 'publicacion')
+    list_per_page = 30
