2ff3613d62abfbb9b39415bef034bce481b556f8
imhsi17 <calderonhuaynates@gmail.com>
2024-11-29 14:05:15 -0500

    Instalación de Pillow y configuración para las imagenes.

--- blog/urls.py
+++ blog/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from main_blog import urls
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('main_blog.urls')),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
