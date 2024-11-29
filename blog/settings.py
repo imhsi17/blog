e82095d6c776887b356d29f7b6c4eb8f723d80d5
imhsi17 <calderonhuaynates@gmail.com>
2024-11-29 14:48:03 -0500

    Creación y migración del modelo publicacion y comentarios.

--- blog/settings.py
+++ blog/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'quill',
+    'django_quill',
     'main_blog'
 ]
 
--- main_blog/models.py
+++ main_blog/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django_quill.fields import QuillField
 
 # Create your models here.
 class Lector(models.Model):
@@ -29,3 +31,51 @@ class Categoria(models.Model):
     def __str__(self) -> str:
         return self.nombre
 
+
+class Publicacion(models.Model):
+    
+    id = models.AutoField('Id', primary_key=True)
+    titulo = models.CharField('Título', blank=False, null=False, max_length=200)
+    contenido = QuillField()
+    fecha_publicacion = models.DateField('Fecha Publicación', auto_now_add=True, auto_now=False)
+    imagen = models.ImageField('Imagen', blank=True, upload_to='publicaciones')
+    estado = models.BooleanField('Publicación Activa/No Activa', default=True)
+    autor = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True)
+    
+    
+    class Meta:
+        verbose_name = 'Publicación'
+        verbose_name_plural = 'Publicaciones'
+    
+    def __str__(self) -> str:
+        return self.titulo
+
+
+class ComentarioAutor(models.Model):
+    
+    id = models.AutoField('Id', primary_key=True)
+    contenido = models.TextField('Contenido', null=False, blank=False)
+    fecha_publicacion = models.DateField('Fecha Publicación', auto_now_add=True, auto_now=False)
+    autor = models.ForeignKey(User, on_delete=models.CASCADE)
+    publicacion = models.ForeignKey(Publicacion, on_delete=models.CASCADE)
+    
+    
+    class Meta:
+        verbose_name = 'Comentario Autor'
+        verbose_name_plural = 'Comentarios Autor'
+
+
+class ComentarioLector(models.Model):
+    
+    id = models.AutoField('Id', primary_key=True)
+    contenido = models.TextField('Contenido', null=False, blank=False)
+    fecha_publicacion = models.DateField('Fecha Publicación', auto_now_add=True, auto_now=False)
+    lector = models.ForeignKey(Lector, on_delete=models.CASCADE)
+    publicacion = models.ForeignKey(Publicacion, on_delete=models.CASCADE)
+    
+    
+    class Meta:
+        verbose_name = 'Comentario Lector'
+        verbose_name_plural = 'Comentarios Lector'
+    
