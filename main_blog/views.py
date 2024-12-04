9846793906312d5f8a6db026e8f0886b0a6f8ed8
imhsi17 <calderonhuaynates@gmail.com>
2024-12-04 18:24:39 -0500

    Cambios en syle.css y views.py.

--- main_blog/views.py
+++ main_blog/views.py
@@ -4,7 +4,6 @@ from .models import Publicacion
 # Create your views here.
 def index(request):
     
-    publicaciones = Publicacion.objects.all().order_by('-fecha_publicacion')
-    
+    publicaciones = Publicacion.objects.filter(estado=True).order_by('-fecha_publicacion')
     
     return render(request, 'index.html', {'publicaciones':publicaciones})
