163c8fbbe1401441e8dbab3641b8c9fec0a537c4
imhsi17 <calderonhuaynates@gmail.com>
2024-12-07 22:44:19 -0500

    Se agregó un apartado de publicaciones recomendadas.

--- main_blog/views.py
+++ main_blog/views.py
@@ -36,12 +36,17 @@ def publicacion(request, id_publicacion):
     
     try:
         publicacion = Publicacion.objects.get(pk=id_publicacion, estado=True)
+        categoria_publicacion = publicacion.categoria
+        
+        otras_publicaciones = Publicacion.objects.filter(categoria=categoria_publicacion,
+                                                         estado=True).exclude(pk=id_publicacion).order_by('-fecha_publicacion')[:3]
     
         categorias = Categoria.objects.filter(estado=True).order_by('-fecha_registro')
         
         context = {
             'publicacion':publicacion,
-            'categorias':categorias
+            'categorias':categorias,
+            'otros':otras_publicaciones
         }
         
         return render(request, 'publicacion.html', context)
