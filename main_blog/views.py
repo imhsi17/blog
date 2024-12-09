f95da228c0c3241c3c308ca165ce2b5d4d34a8f6
imhsi17 <calderonhuaynates@gmail.com>
2024-12-08 19:46:46 -0500

    Se agregó el apartado para mostrar comentarios guardados en publicacion.html.

--- main_blog/views.py
+++ main_blog/views.py
@@ -39,14 +39,22 @@ def publicacion(request, id_publicacion):
     try:
         publicacion = Publicacion.objects.get(pk=id_publicacion, estado=True)
         categoria_publicacion = publicacion.categoria
-        otras_publicaciones = Publicacion.objects.filter(categoria=categoria_publicacion,
-                                                         estado=True).exclude(pk=id_publicacion).order_by('-fecha_publicacion')[:3]
+        otras_publicaciones = Publicacion.objects.filter(
+            categoria=categoria_publicacion,
+            estado=True).exclude(
+                pk=id_publicacion).order_by('-fecha_publicacion')[:3]
         categorias = Categoria.objects.filter(estado=True).order_by('-fecha_registro')
+        autor_comentarios = ComentarioAutor.objects.filter(
+            publicacion=publicacion).order_by('-fecha_publicacion')
+        lector_comentarios = ComentarioLector.objects.filter(
+            publicacion=publicacion).order_by('-fecha_publicacion')
         
         context = {
             'publicacion':publicacion,
             'categorias':categorias,
-            'otros':otras_publicaciones
+            'otros':otras_publicaciones,
+            'autor_comentarios':autor_comentarios,
+            'lector_comentarios':lector_comentarios
         }
         
         if request.method=='POST' and request.user.is_authenticated:
