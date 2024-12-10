c1a5c8db7c18d0a3ae0e62121392da93367fe9d1
imhsi17 <calderonhuaynates@gmail.com>
2024-12-10 00:01:05 -0500

    Se agregarón las urls para iniciar y cerrar sesion.

--- main_blog/urls.py
+++ main_blog/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import index, categoria, publicacion, registro_lector
+from .views import (index, categoria, publicacion,
+                    registro_lector, inicio_sesion_autor,
+                    cerrar_sesion)
 
 urlpatterns = [
     path('', index, name='index'),
+    path('login/', inicio_sesion_autor, name='login'),
+    path('logout/', cerrar_sesion, name='logout'),
     path('categoria/<str:nombre>', categoria, name='categoria'),
     path('publicacion/<int:id_publicacion>', publicacion, name='publicacion'),
     path('registro/', registro_lector, name='registro')
